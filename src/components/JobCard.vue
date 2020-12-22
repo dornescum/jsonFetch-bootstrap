2a7c1ab050a0f7d6d47aa8f74d793315a59282bf
dornescum <dornescu@gmail.com>
2020-12-22 20:53:05 +0200

    varianta cu totul generat din json

--- src/components/JobCard.vue
+++ src/components/JobCard.vue
@@ -1,11 +1,29 @@
 <template>
-$END$
+  <b-col md="4">
+    <b-card
+      :title="name"
+     :img-src="picture"
+      img-alt="picture"
+      img-top
+      tag="article"
+      style="max-width: 20rem;"
+      class="mb-2 mt-4"
+      :footer="footer"
+    >
+
+      <b-card-text v-text="text">
+<!--        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, temporibus.-->
+      </b-card-text>
+      <b-button variant="primary">Apply</b-button>
+    </b-card>
+  </b-col>
 </template>
 
 <script>
 export default {
-name: "JobCard"
-}
+  name: "JobCard",
+  props: ['name', "footer", "picture", "text"]
+};
 </script>
 
 <style scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <b-container>
+    <b-row align-v="center">
+      <job-card v-for="job in jobs" :key="job.id" md="2"
+                :name="job.name"
+                :footer="job.footer"
+                :picture="job.img"
+                :text="job.text"
+      >
+      </job-card>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import JobCard from "@/components/JobCard";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      jobs: []
+    };
+  },
   components: {
-    HelloWorld
+    JobCard
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      const response = await fetch("jobs.json");
+      const value = await response.json();
+      this.jobs = value;
+      console.log(value);
+    }
   }
+
 };
 </script>
